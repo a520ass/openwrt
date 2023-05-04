e4b3079c1a1fe58cdf0fedc42192ce8f56ecec34
a520a <a520ass@hotmail.com>
2023-05-04 16:50:43 +0800

    HACK: Add ufi

--- target/linux/msm89xx/image/msm8916.mk
+++ target/linux/msm89xx/image/msm8916.mk
@@ -7,3 +7,12 @@ define Device/longcheer-l8150
 	CMDLINE := "earlycon console=ttyMSM0,115200 root=/dev/mmcblk0p31 rw rootwait"
 endef
 TARGET_DEVICES += longcheer-l8150
+
+define Device/thwc-ufi001c
+       DEVICE_VENDOR := THWC
+       DEVICE_MODEL := UFI001C
+       SOC := msm8916
+       CMDLINE := "earlycon console=ttyMSM0,115200 root=/dev/mmcblk0p14 rw rootwait"
+endef
+TARGET_DEVICES = thwc-ufi001c
+
