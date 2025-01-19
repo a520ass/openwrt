f2854190c57bff1c430ec0a8563fe9ec8311643b
a520a <a520ass@hotmail.com>
2025-01-19 19:47:17 +0800

    msm8916

---
+++ target/linux/msm89xx/image/msm8916.mk
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: GPL-2.0-only
+
+define Device/longcheer-l8150
+	DEVICE_VENDOR := Longcheer
+	DEVICE_MODEL := L8150
+	SOC := msm8916
+	ROOT_BLKDEV := "/dev/mmcblk0p31"
+endef
+TARGET_DEVICES += longcheer-l8150
+
+define Device/thwc-uf896
+	DEVICE_VENDOR := THWC
+	DEVICE_MODEL := uf896
+	SOC := msm8916
+	ROOT_BLKDEV := "/dev/mmcblk0p27"
+endef
+TARGET_DEVICES += thwc-uf896
+
+define Device/thwc-ufi001c
+	DEVICE_VENDOR := THWC
+	DEVICE_MODEL := ufi001c
+	SOC := msm8916
+	ROOT_BLKDEV := "/dev/mmcblk0p14"
+endef
+TARGET_DEVICES += thwc-ufi001c
